c45a05b47e263ec1161ad772378d970057a8793b
Sujit Kumar <147229100+SujitSingh521@users.noreply.github.com>
2025-02-01 12:48:52 +0000

    updated

--- react-props/src/components/Page.jsx
+++ react-props/src/components/Page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Page = ({ title, content }) => {
+const Page = ({ title, content,Image }) => {
   return (
     <div className="page">
       <h1>{title}</h1>
       <p>{content}</p>
+      <h2>{Image}</h2>
     </div>
   );
 };
--- react-props/src/pages/Home.jsx
+++ react-props/src/pages/Home.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import Page from "../components/Page";
 
 const Home = () => {
-  return <Page title="Welcome to Home Page" content="This is the home page of our single-page application." />;
+  return (
+    <div className="home">
+      <div className="home-content">
+        <div className="home-text">
+          <Page 
+            title="Welcome to Our Website" 
+            content="We provide the best solutions for your business. Explore our services and get in touch!" 
+          />
+          <button className="cta-button">Learn More</button>
+        </div>
+        <div className="home-image">
+          <img 
+            src="https://media.istockphoto.com/id/1273320106/vector/deep-space-planetary-rings-abstract-background.jpg?s=612x612&w=0&k=20&c=vkV-9H5VcRpGWG7WHVuIZRIPnLOhCWrwj8SBebTgLN0=" 
+            alt="Home Banner" 
+            className="home-banner" 
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
