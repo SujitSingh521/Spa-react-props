27b2fd945de0c79c092ab39816effd52332cd762
Sujit Kumar <147229100+SujitSingh521@users.noreply.github.com>
2025-02-02 04:25:31 +0000

    LearnProps updated

---
+++ props/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LearnProps from "./components/LearnProps";
+
+function App() {
+  return (
+    <div className="App">
+      <LearnProps name="Sujit Kumar" />
+    </div>
+  );
+}
+
+export default App;
